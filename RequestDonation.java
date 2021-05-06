2672bce0c245655e6c3db05fca08a164fba0361b
andreasftk <68335410+andreasftk@users.noreply.github.com>
2021-05-06 10:29:17 +0300

    Create RequestDonation.java

---
+++ RequestDonation.java
@@ -0,0 +1,9 @@
+public class RequestDonation {
+    private Entity entity;
+    private double quantity;
+
+    RequestDonation(Entity entity,double quantity){
+        this.entity=entity;
+        this.quantity=quantity;
+    }
+}
