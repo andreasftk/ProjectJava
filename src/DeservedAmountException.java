53d6410e083a2ca4161787730d3c96cd8c119280
andreasftk <68335410+andreasftk@users.noreply.github.com>
2021-06-08 12:38:18 +0300

    Delete src directory

--- src/DeservedAmountException.java
+++
@@ -1,5 +0,0 @@
-public class DeservedAmountException extends ProjectExceptions {
-    public void getExceptionInfo() {
-        System.out.println("The amount you asked for exceeds the amount you deserve.");
-    }
-}
--- src/FalseAmountException.java
+++
@@ -1,3 +0,0 @@
-public class FalseAmountException extends ProjectExceptions{
-    public void getExceptionInfo(){System.out.println("The amount you asked for exceeds the maximum already available\n\t\t\tor\nThe amount asked for has a negative value.");}
-}
